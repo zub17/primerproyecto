c97040222a53cebdb8d1136228b60a8ab5cd94d4
zub17 <jmrd17@gmail.com>
2022-10-12 11:45:13 -0500

    se agregaron alertas

--- js/POKEMON.js
+++ js/POKEMON.js
@@ -1,4 +1,12 @@
-alert("hola XD")
+function iniciarJuego() {
+    let botonMascotaJugador = document.getElementById('boton-mascota')
+    botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador)
+}
 
-let  botonMascotaJugador = document.getElementById ("boton-mascota")
-botonMascotaJugador.addEventListener("click" , seleccionarMascotaJugador)
+function seleccionarMascotaJugador() {
+    alert('SELECCIONASTE A TU POKEMON')
+}
+
+let botonMascotaJugador = document.getElementById('boton-mascota')
+botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador)
+window.addEventListener('load', iniciarJuego)
